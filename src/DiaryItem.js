f30232cb92b08acfbdd2412dab6a788803732183
jeonginkwon <kwjddls1919@naver.com>
2022-06-08 23:39:21 +0900

    first

---
+++ src/DiaryItem.js
@@ -0,0 +1,83 @@
+import React, { useState, useRef, useEffect } from "react";
+
+const DiaryItem = ({
+  onEdit,
+  onRemove,
+  author,
+  content,
+  created_date,
+  emotion,
+  id,
+}) => {
+  useEffect(() => {
+    console.log(`${id}번 째 아이템 렌더`);
+  });
+
+  const [isEdit, SetIsEdit] = useState(false);
+  const toggleIsEdit = () => SetIsEdit(!isEdit);
+
+  const [localContent, setLocalContent] = useState(content);
+  const localContentInput = useRef();
+
+  const handleRomove = () => {
+    if (window.confirm(`${id}번째 일기를 정말 삭제하시겠습니까?`)) {
+      onRemove(id);
+    }
+  };
+
+  const handleQuitEdit = () => {
+    SetIsEdit(false);
+    setLocalContent(content);
+  };
+
+  const handleEdit = () => {
+    if (localContent.length < 5) {
+      localContentInput.current.focus();
+      return;
+    }
+
+    if (window.confirm(`${id}번 째 일기를 수정하시겠습니까?`)) {
+      onEdit(id, localContent);
+      toggleIsEdit();
+    }
+  };
+
+  return (
+    <div className="DiaryItem">
+      <div className="info">
+        <span className="author_info">
+          작성자 : {author} | 감정점수 : {emotion}
+        </span>
+        <br />
+        <span className="date">{new Date(created_date).toLocaleString()}</span>
+      </div>
+      <div className="content">
+        {isEdit ? (
+          <>
+            <textarea
+              ref={localContentInput}
+              value={localContent}
+              onChange={(e) => setLocalContent(e.target.value)}
+            ></textarea>
+          </>
+        ) : (
+          <>{content}</>
+        )}
+      </div>
+
+      {isEdit ? (
+        <>
+          <button onClick={handleQuitEdit}>수정 취소</button>
+          <button onClick={handleEdit}>수정 완료</button>
+        </>
+      ) : (
+        <>
+          <button onClick={handleRomove}>삭제하기</button>
+          <button onClick={toggleIsEdit}>수정하기</button>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default React.memo(DiaryItem);
---
+++ src/DiaryList.js
@@ -0,0 +1,25 @@
+import { useContext } from "react";
+import { DiaryDispatchContext, DiaryStateContext } from "./App";
+import DiaryItem from "./DiaryItem";
+
+const DiaryList = () => {
+  const { onEdit, onRemove } = useContext(DiaryDispatchContext);
+  const diaryList = useContext(DiaryStateContext);
+  return (
+    <div className="DiaryList">
+      <h2>일기 리스트</h2>
+      <h4>{diaryList.length}개의 일기가 있습니다.</h4>
+      <div>
+        {diaryList.map((it) => (
+          <DiaryItem key={it.id} {...it} onRemove={onRemove} onEdit={onEdit} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+DiaryList.defaultProps = {
+  diaryList: [],
+};
+
+export default DiaryList;
